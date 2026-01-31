8d82937dd76d8f81be7f0a676c0ccfc587363cdb
courtneycarroll2 <courtneycarroll2@gmail.com>
2026-01-31 11:41:49 -0700

    Added function to do QC on genomic data in qc.R.

--- R/qc.R
+++ R/qc.R
@@ -1,7 +1,36 @@
 # qc.R contains code to do quality control on the data and report missing rates of variables.
 # Also plan to add multiple imputation
 
-qc_genomic <- function(genomic_data) {
+
+# Identifies samples with ≥X% genomic/mutation data missing; percentage specified in deserialized JSON specs file under cohort_model_specs$qc$proportion_missing_rm_variable, if 1) genomic_data is not NULL, and 2) cohort_model_specs$qc$proportion_missing_rm_variable exists.
+# If the two criteria above are met, this function returns a list containing 1) a vector of sample IDs to retain and 2) stats to add to a qc file. Otherwise, it returns NULL.
+qc_genomic <- function(genomic_data, cohort_model_specs) {
+  # Find samples missing < specified proportion of genomic data
+  if (!is.null(cohort_model_specs$qc$proportion_missing_rm_variable) & !is.null(genomic_data)) {
+    pct <- cohort_model_specs$qc$proportion_missing_rm_variable
+    keep_indices <- (colSums(is.na(genomic_data[,2:ncol(genomic_data)]))/nrow(genomic_data)) < pct
+    keep_samples <- colnames(genomic_data[,2:ncol(genomic_data)][keep_indices])
+    
+    # qc output:
+    stage <- paste0("rm_missing_genomic_gt_", pct, "_pct")
+    n_in <- ncol(genomic_data)-1
+    n_keep <- length(keep_samples)
+    n_rm <- n_in - n_keep
+    pct_rm <- round((n_rm / n_in) * 100, 2)
+    description <- paste0("removed samples missing ≥ ", pct*100 , "% genomic data")
+    qc_row <- c(stage, n_in, n_keep, n_rm, pct_rm, description)
+    return(
+      list(
+        keep_samples = keep_samples,
+        qc_row = qc_row
+      )
+    )
+  } 
+  
+  # otherwise, return NULL
+  else {
+    return(NULL)
+  }
   
 }
 
